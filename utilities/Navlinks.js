08918f55f02067f62b06e7e58f575faad0e15984
Abdullah Al-Akib <abdullahalakib12@gmail.com>
2022-10-06 22:51:41 +0600

    another summary

---
+++ utilities/Navlinks.js
@@ -0,0 +1,37 @@
+const mainLinks = [
+  {
+    nav_name: "Home",
+    nav_link: "/",
+  },
+  {
+    nav_name: "Destinations",
+    nav_link: "/Destinations",
+  },
+  {
+    nav_name: "Hotels",
+    nav_link: "/SearchHotels",
+  },
+  {
+    nav_name: "Maldives Package",
+    nav_link: "/MaldivesPackage",
+  },
+  {
+    nav_name: "Turkey Package",
+    nav_link: "/TurkeyPackage",
+  },
+  {
+    nav_name: "About Us",
+    nav_link: "/AboutUs",
+  },
+  {
+    nav_name: "FAQ",
+    nav_link: "/#faq",
+  },
+  {
+    nav_name: "Contacts",
+    nav_link: "/#contacts",
+  },
+];
+
+
+export {  mainLinks };
---
+++ utilities/VideoContentData.js
@@ -0,0 +1,32 @@
+const TouringSectionContent = {
+    heading: "Step outside your comfort zone!",
+    subHeading: "A world of dream awaits",
+    videoID: "gD5s2Wi_lj4",
+    backgroundColor: '#EFF3FC'
+};
+
+const VirtualAssistantSectionContent = {
+    heading: "Introducing Your Virtual Travel Assistant",
+    subHeading: "First Time Ever In Bangladesh",
+    videoID: "GRMNEu7hQtQ",
+    backgroundColor: ''
+}
+
+const AboutUsContent = {
+    heading: "BUDGET EXCURSIONS",
+    subHeading: "Making Dreams come true at a price no one can beat!",
+    videoID: "EAUzBR6jS6E",
+    backgroundColor: ''
+}
+
+const IntroContent = {
+    heading: "BUDGET EXCURSIONS",
+    subHeading: "",
+    videoID: "VD_n33gqXag",
+    backgroundColor: ''
+}
+
+
+
+
+export {TouringSectionContent, VirtualAssistantSectionContent, AboutUsContent, IntroContent };
